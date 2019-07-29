25aff34a14e6bd99f01b5e3bebe21b40eaa2a29e
Marco Antonio Augusto de Andrade <marco.andrade@ifro.edu.br>
2019-07-29 16:06:02 -0400

    Corrigindo bugs no JavaScript

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -1,57 +1,37 @@
-function changeTheme(){
-    
-    verifyTheme();
-    let cookies = document.cookie.split(';');
-    
-    for (i = 0; i < cookies.length; i++){
-        
-        if (cookies[i] == "style=dark") {
-            style.href = '/assets/css/light.css';
-            document.cookie = "style=light";
-            console.log("Mudando para Light");
-        }
-        if (cookies[i] == " style=dark") {
-            style.href = '/assets/css/light.css';
-            document.cookie = "style=light";
-            console.log("Mudando para Light");
-        }
-        if (cookies[i] == "style=ligth"){
-            style.href = '/assets/css/dark.css';
-            document.cookie = "style=dark";
-            console.log("Mudando para Dark");
-        }
-        if (cookies[i] == " style=ligth"){
-            style.href = '/assets/css/dark.css';
-            document.cookie = "style=dark";
-            console.log("Mudando para Dark");
-        }
-    }
-}
-
-// OK
-function verifyTheme(){
-    
-    let cookies = document.cookie.split(';');
-    let count = 0;
-    for (i = 0; i < cookies.length; i++){
-        if (cookies[i] == "style=ligth" || cookies[i] == " style=light" || cookies[i] == "style=dark" || cookies[i] == " style=dark"){
-            count++;
-        } 
-    }
-    if (count == 0) document.cookie = "style=dark";
-    console.log(count);
-}
-
 function setCookie(){
     document.cookie = "cookies=true";
     verifyCookie();
 }
 
 function verifyCookie(){
-    let cookies = document.cookie.split(';');
+    
+    if (document.cookie.indexOf("cookies") >= 0){
+        document.querySelector(".cookies").style.display = "none";
+    }
+}
+
+function verifyTheme(){
+    
+    if (document.cookie.indexOf("style") < 0){
+        document.cookie = "style=dark";
+    }
+}
+
+
+function changeTheme(){
+    
+    verifyTheme();
+    let value = '; ' + document.cookie;
+    let cookies = value.split('; ');
+    
     for (i = 0; i < cookies.length; i++){
-        if (cookies[i] == "cookies=true"){
-            document.querySelector(".cookies").style.display = "none";
+        
+        if (cookies[i] == "style=dark"){
+            document.cookie = "style=light";
+            style.href = '/assets/css/light.css';
+        } else if (cookies[i] == "style=light"){
+            document.cookie = "style=dark";
+            style.href = '/assets/css/dark.css';
         }
     }
-}  
+}
